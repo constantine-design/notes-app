bb167f25612a720d8506702fc1c4455b61567a32
konstantinreso <88545523+konstantinreso@users.noreply.github.com>
2021-08-06 18:13:57 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -63,76 +63,76 @@ function App() {
 			>
 
         <ul
-			className="nav nav-pills mb-3 justify-content-center"
-			id="pills-tab"
-			role="tablist"
-		>
-			<li
-				className="nav-item"
-				role="presentation"
-			>
-				<button
-					className="nav-link active"
-					id="pills-home-tab"
-					data-bs-toggle="pill"
-					data-bs-target="#pills-home"
-					type="button"
-					role="tab"
-					aria-controls="pills-home"
-					aria-selected="true"
+					className="nav nav-pills mb-3 justify-content-center"
+					id="pills-tab"
+					role="tablist"
 				>
-					Current
-				</button>
-			</li>
-			<li
-				className="nav-item"
-				role="presentation"
-			>
-				<button
-					className="nav-link"
-					id="pills-profile-tab"
-					data-bs-toggle="pill"
-					data-bs-target="#pills-profile"
-					type="button" role="tab"
-					aria-controls="pills-profile"
-					aria-selected="false"
-				>
-					Completed
-				</button>
-			</li>
+					<li
+						className="nav-item"
+						role="presentation"
+					>
+						<button
+							className="nav-link active"
+							id="pills-home-tab"
+							data-bs-toggle="pill"
+							data-bs-target="#pills-home"
+							type="button"
+							role="tab"
+							aria-controls="pills-home"
+							aria-selected="true"
+						>
+							Current
+						</button>
+					</li>
+					<li
+						className="nav-item"
+						role="presentation"
+					>
+						<button
+							className="nav-link"
+							id="pills-profile-tab"
+							data-bs-toggle="pill"
+							data-bs-target="#pills-profile"
+							type="button" role="tab"
+							aria-controls="pills-profile"
+							aria-selected="false"
+						>
+							Completed
+						</button>
+					</li>
 
         </ul>
 
         <div className="tab-content" id="pills-tabContent">
-			<div
-				className="tab-pane fade show active"
-				id="pills-home"
-				role="tabpanel"
-				aria-labelledby="pills-home-tab"
-			>
-				<RenderNotes
-					notes = { notes }
-					setNotes = { setNotes }
-					showComplete = { false }
-					count = { count }
-					setCount = { setCount }
-				/>
-			</div>
+					<div
+						className="tab-pane fade show active"
+						id="pills-home"
+						role="tabpanel"
+						aria-labelledby="pills-home-tab"
+					>
+						<RenderNotes
+							notes = { notes }
+							setNotes = { setNotes }
+							showComplete = { false }
+							count = { count }
+							setCount = { setCount }
+						/>
+					</div>
 
-			<div
-				className="tab-pane fade"
-				id="pills-profile"
-				role="tabpanel"
-				aria-labelledby="pills-profile-tab"
-			>
-				<RenderNotes
-					notes = { notes }
-					setNotes = { setNotes }
-					showComplete = { true }
-					count = { count }
-					setCount = { setCount }
-				/>
-			</div>
+					<div
+						className="tab-pane fade"
+						id="pills-profile"
+						role="tabpanel"
+						aria-labelledby="pills-profile-tab"
+					>
+						<RenderNotes
+							notes = { notes }
+							setNotes = { setNotes }
+							showComplete = { true }
+							count = { count }
+							setCount = { setCount }
+						/>
+					</div>
         </div>
       </div>
 
